297f00e7baf304c23419d8b1c76d7a9c01dd90ff
Micah Johnston <wyndclaw@gmail.com>
2011-07-10 20:34:31 -0600

    fix: identifiers are properly restricted so they don't contain whitespace, closing parens and braces, etc.

--- Parser.hs
+++ Parser.hs
@@ -7,13 +7,12 @@ import Control.Applicative hiding ((<|>), many)
 parser :: String -> Either ParseError AST
 parser = parse (spaces *> expr <* eof) ""
 
-expr, chain, term, paren, block, symbol :: Parser AST
-expr   = chain <|> term
-chain  = foldl Juxtapose Self <$> many term
+expr, term, paren, block, symbol :: Parser AST
+expr   = foldl Juxtapose Self <$> many term
 term   = paren <|> block <|> symbol
-paren  = Juxtapose Self <$> (lexeme (char '(') *> expr <* lexeme (char ')'))
+paren  = lexeme (char '(') *> expr <* lexeme (char ')')
 block  = Block <$> (lexeme (char '{') *> expr <* lexeme (char '}'))
-symbol = Symbol <$> lexeme (many1 (noneOf "\"") <|> char '"' *> many1 (noneOf "\"") <* char '"')
+symbol = Symbol <$> lexeme (many1 (noneOf "\"{}() \t\n") <|> char '"' *> many1 (noneOf "\"") <* char '"')
 
 lexeme :: Parser a -> Parser a
 lexeme p = p <* spaces
